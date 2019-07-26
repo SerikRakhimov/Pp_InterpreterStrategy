6f9014cedefd57a98e4fd1faca57cf34e706e45c
RSB <s_astana@mail.ru>
2019-07-26 08:41:10 +0600

    first commit

---
+++ Pp_Interpreter/Pp_Interpreter/Pp_Interpreter/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Interpreter(Интерпретатор) 
+// Приведен пример интерпретатора для операций сложения и вычитания.
+// Добавить в интерпретатор операцию умножения.
+
+namespace Pp_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            // определяем набор переменных
+            int a = 2;
+            int x = 5;
+            int y = 8;
+            int z = 2;
+
+            // добавляем переменные в контекст
+            context.SetVariable("a", a);
+            context.SetVariable("x", x);
+            context.SetVariable("y", y);
+            context.SetVariable("z", z);
+            // создаем объект для вычисления выражения (x + y - z) * a
+            IExpression expression =
+            new IncreaseExpression(
+                new SubtractExpression(
+                    new AddExpression(new NumberExpression("x"), new NumberExpression("y")),
+                    new NumberExpression("z")),
+                new NumberExpression("a"));
+
+            int result = expression.Interpret(context);
+            Console.WriteLine("Результат: {0}", result);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Context
+    {
+        Dictionary<string, int> variables;
+        public Context()
+        {
+            variables = new Dictionary<string, int>();
+        }
+        // получаем значение переменной по ее имени
+        public int GetVariable(string name)
+        {
+            return variables[name];
+        }
+
+        public void SetVariable(string name, int value)
+        {
+            if (variables.ContainsKey(name))
+                variables[name] = value;
+            else
+                variables.Add(name, value);
+        }
+    }
+    // интерфейс интерпретатора
+    interface IExpression
+    {
+        int Interpret(Context context);
+    }
+    // терминальное выражение
+    class NumberExpression : IExpression
+    {
+        string name; // имя переменной
+        public NumberExpression(string variableName)
+        {
+            name = variableName;
+        }
+        public int Interpret(Context context)
+        {
+            return context.GetVariable(name);
+        }
+    }
+    // нетерминальное выражение для сложения
+    class AddExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public AddExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) + rightExpression.Interpret(context);
+        }
+    }
+
+    // нетерминальное выражение для вычитания
+    class SubtractExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public SubtractExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) - rightExpression.Interpret(context);
+        }
+    }
+
+    // нетерминальное выражение для умножения
+    class IncreaseExpression : IExpression
+    {
+        IExpression leftExpression;
+        IExpression rightExpression;
+
+        public IncreaseExpression(IExpression left, IExpression right)
+        {
+            leftExpression = left;
+            rightExpression = right;
+        }
+
+        public int Interpret(Context context)
+        {
+            return leftExpression.Interpret(context) * rightExpression.Interpret(context);
+        }
+    }
+    
+}
---
+++ Pp_Strategy/Pp_Strategy/Pp_Strategy/Program.cs
@@ -0,0 +1,116 @@
+﻿using Pp_Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pp_Strategy
+{
+    // Контекст определяет интерфейс, представляющий интерес для клиентов.
+    class Context
+    {
+        // Контекст хранит ссылку на один из объектов Стратегии. Контекст не
+        // знает конкретного класса стратегии. Он должен работать со всеми
+        // стратегиями через интерфейс Стратегии.
+        private IStrategy _strategy;
+
+        public Context()
+        { }
+
+        // Обычно Контекст принимает стратегию через конструктор, а также
+        // предоставляет сеттер для её изменения во время выполнения.
+        public Context(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Обычно Контекст позволяет заменить объект Стратегии во время
+        // выполнения.
+        public void SetStrategy(IStrategy strategy)
+        {
+            this._strategy = strategy;
+        }
+
+        // Вместо того, чтобы самостоятельно реализовывать множественные
+        // версии алгоритма, Контекст делегирует некоторую работу объекту
+        // Стратегии.
+        public void DoSomeBusinessLogic()
+        {
+            var result = this._strategy.DoAlgorithm();
+
+            Console.WriteLine($"{result}");
+
+        }
+    }
+
+
+    // Интерфейс Стратегии объявляет операции, общие для всех поддерживаемых
+    // версий некоторого алгоритма.
+    //
+    // Контекст использует этот интерфейс для вызова алгоритма, определённого
+    // Конкретными Стратегиями.
+    public interface IStrategy
+    {
+        object DoAlgorithm();
+    }
+
+    // Конкретные Стратегии реализуют алгоритм, следуя базовому интерфейсу
+    // Стратегии. Этот интерфейс делает их взаимозаменяемыми в Контексте.
+    class ConcreteStrategyDragon : IStrategy
+    {
+        public object DoAlgorithm()
+        {
+            var info = "бежать, прыгать";
+            return info;
+        }
+    }
+
+    class ConcreteStrategyVelican : IStrategy
+    {
+        public object DoAlgorithm()
+        {
+            var info = "cтрелять";
+            return info;
+        }
+    }
+
+    class ConcreteStrategyTurelyia : IStrategy
+    {
+        public object DoAlgorithm()
+        {
+            var info = "уворачиваться";
+            return info;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Клиентский код выбирает конкретную стратегию и передаёт её в
+            // контекст. Клиент должен знать о различиях между стратегиями,
+            // чтобы сделать правильный выбор.
+            var context = new Context();
+
+            Console.WriteLine("Стратегия битвы с драконом:");
+            context.SetStrategy(new ConcreteStrategyDragon());
+            context.DoSomeBusinessLogic();
+            Console.WriteLine();
+
+            Console.WriteLine("Стратегия битвы с великаном:");
+            context.SetStrategy(new ConcreteStrategyVelican());
+            context.DoSomeBusinessLogic();
+            Console.WriteLine();
+
+            Console.WriteLine("Стратегия битвы с турелью:");
+            context.SetStrategy(new ConcreteStrategyTurelyia());
+            context.DoSomeBusinessLogic();
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+        }
+    }
+
+}
